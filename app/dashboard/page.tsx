774cd398c7c71a8d1ed82710e83672054803f630
Tomas <tomasluquegonzalez@gmail.com>
2026-01-07 11:03:33 +0100

    remove unused component + add dope loading page for dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,8 +2,9 @@ import { getServerSession } from "next-auth";
 import { prisma } from "@/lib/prisma";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
-import { ProfileForm } from "./ProfileForm";
+// import { ProfileForm } from "./ProfileForm";
 import { SignOutButton } from "@/components/buttons";
+import { updateUser } from "./actions";
 
 export default async function Dashboard() {
     const session = await getServerSession(authOptions);
@@ -23,7 +24,22 @@ export default async function Dashboard() {
         <>
             <h1>Dashboard</h1>
             <SignOutButton />
-            <ProfileForm user={user} />
+            {/* <ProfileForm user={user} /> */}
+            <div>
+                <h2>Edit Your Profile</h2>
+                <form action={updateUser}>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" defaultValue={user?.name ?? ""} />
+                    <label htmlFor="bio">Bio</label>
+                    <textarea name="bio" cols={30} rows={10} defaultValue={user?.bio ?? ""}></textarea>
+                    <label htmlFor="age">Age</label>
+                    <input type="text" name="age" defaultValue={user?.age ?? 0} />
+                    <label htmlFor="image">Profile Image URL</label>
+                    <input type="text" name="image" defaultValue={user?.image ?? ""} />
+
+                    <button type="submit">Save</button>
+                </form>
+            </div>
         </>
     );
 }
